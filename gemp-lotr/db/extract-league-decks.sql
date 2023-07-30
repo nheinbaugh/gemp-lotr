5e55abb7d0c7bdb9be48580c31e1f975c5b9b456
Christian 'ketura' McCarty <hyperlink.teltura@gmail.com>
2023-07-30 18:03:54 -0500

    fixing underscore handling for names in the league deck extraction db script

--- gemp-lotr/db/extract-league-decks.sql
+++ gemp-lotr/db/extract-league-decks.sql
@@ -1,15 +1,15 @@
 
 
 
-SET @player = 'CoS', @rank=0;
+SET @player = 'sempolPL', @rank=0;
 
 SELECT 
 	 @player AS player
 	,CASE WHEN winner = @player THEN winner_deck_name  ELSE loser_deck_name  END AS deck_name
 	,CONCAT('https://play.lotrtcgpc.net/share/deck?id=',
 		CASE 
-			WHEN winner = @player THEN TO_BASE64(CONCAT(REPLACE(winner, '_', '%5F'), '|', winner_deck_name))
-			ELSE TO_BASE64(CONCAT(REPLACE(loser, '_', '%5F'), '|', loser_deck_name)) 
+			WHEN winner = @player THEN TO_BASE64(CONCAT(winner, '|', winner_deck_name))
+			ELSE TO_BASE64(CONCAT(loser, '|', loser_deck_name)) 
 		END) AS DeckURL
 	,CONCAT('[url=https://play.lotrtcgpc.net/gemp-lotr/game.html?replayId=',
 		CASE 
@@ -21,9 +21,9 @@ INNER JOIN player p
 	ON p.name = @player
 INNER JOIN deck d 
 	ON d.player_id = p.id 
-	AND (d.name = winner_deck_name OR d.name = loser_deck_name) 
-WHERE tournament = 'March Weekend Qualifier - PC-Fellowship - Serie 1'
-	AND start_date > '2023-03-22'
+	AND (d.name = winner_deck_name OR d.name = loser_deck_name OR d.name = 'denethor / tentacle V2') 
+WHERE tournament = 'Constructed - Weekend Qualifier PC-Expanded - Serie 1'
+	AND start_date > '2023-07-20'
 	AND (winner = @player OR loser = @player)
 ORDER BY gh.id;
 
